52b98420081803b09156bf6c72f465db36b05dcb
canguang.liu <canguang.liu@unre.com>
2019-03-08 16:38:32 +0800

    关联主界面与子界面

---
+++ DouYuZB/DouYuZB/Classs/Main/MainViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MainViewController.swift
+//  DouYuZB
+//
+//  Created by canguang on 2019/2/28.
+//  Copyright © 2019 canguang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        addChildVc(storyName: "Home")
+        addChildVc(storyName: "Live")
+        addChildVc(storyName: "Follow")
+        addChildVc(storyName: "Profile")
+    }
+    
+    private func addChildVc(storyName : String){
+        let childvc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChild(childvc)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
